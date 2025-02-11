131c422680d0941a217149b53f4310586d5e8212
Lukas Olson <lukasdragon@live.ca>
2025-02-10 18:32:10 -0800

    Improved setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="0.1.0",
     author="Lukas G. Olson",
     author_email="olson@student.ubc.ca",
-    description="A short description of your package.",
-    long_description=open("README.md", encoding="utf-8").read(),
+    description="A dynamic, easy-to-use .ini configuration system built for humans.",
+    long_description=open("readme.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/lukasgolson/dyncfg",
     packages=find_packages(),  # Automatically discover all packages and subpackages
